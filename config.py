aa4f6d79f37b2b39534abc769e17528dc2c47200
Münker, Simon <muenker@uni-trier.de>
2024-03-21 11:57:22 +0100

    add new experiment interation

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ import typing
 
 @dataclasses.dataclass
 class Config:
+    version: str = '0.0.2'
+
     template_path: str = './template.txt'
     final_dataset_path: str = './data/dataset'
 
@@ -20,5 +22,8 @@ class Config:
 
     def __post_init__(self):
         self.template: str = open(self.template_path).read().strip()
+
+        self.raw_data_dir = f'{self.raw_data_dir}/{self.version}'
+        self.final_dataset_path = f'{self.final_dataset_path}-{self.version}'
 
         pathlib.Path(self.raw_data_dir).mkdir(parents=True, exist_ok=True)

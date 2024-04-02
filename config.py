4abff6d8b2f090511f6520b841245998ca065e85
Münker, Simon <muenker@uni-trier.de>
2024-04-02 11:13:19 +0200

    update experiment structure

--- config.py
+++ config.py
@@ -5,28 +5,32 @@ import typing
 
 @dataclasses.dataclass
 class Config:
-    version: str = '0.0.2'
+    version: str = '0.0.3'
 
     template_path: str = './template.txt'
-    final_dataset_path: str = './data/dataset'
-
-    raw_data_dir: str = './data/raw'
+    dataset_name: str = 'dataset'
+    data_dir: str = './data'
+    report_dir: str = './report'
 
     topic: str = 'Protecting of personal information regarding data privacy'
+
     models: typing.List[str] = dataclasses.field(default_factory=lambda: [
-        # 'llama2:70b',
-        'mixtral:8x7b',
-        # 'falcon:40b',
-        # 'qwen:72b'
+        'llama2:70b-chat-q6_K',
+        'falcon:40b-instruct-q5_1',
+        'mixtral:8x7b-instruct-v0.1-q6_K',
+        'qwen:72b-chat-v1.5-q6_K'
     ])
 
-    samples_per_permutation: int = 500
+    samples_per_permutation: int = 3500
     n_grams_analysis: int = 1
 
     def __post_init__(self):
         self.template: str = open(self.template_path).read().strip()
 
-        self.raw_data_dir = f'{self.raw_data_dir}/{self.version}'
-        self.final_dataset_path = f'{self.final_dataset_path}-{self.version}'
+        self.data_dir = f'{self.data_dir}/{self.version}'
+        self.data_raw_dir = f'{self.data_dir}/raw'
+        self.report_dir = f'{self.report_dir}/{self.version}'
 
-        pathlib.Path(self.raw_data_dir).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(self.data_dir).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(self.data_raw_dir).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(self.report_dir).mkdir(parents=True, exist_ok=True)
